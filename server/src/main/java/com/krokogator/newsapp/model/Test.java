dcc6716b744064fb47560e1ae087ecf0da28096d
Krokogator <michal26262@gmail.com>
2019-01-18 18:18:19 +0100

    Test client-server connection

---
+++ server/src/main/java/com/krokogator/newsapp/model/Test.java
@@ -0,0 +1,18 @@
+package com.krokogator.newsapp.model;
+
+public class Test {
+    private String value;
+
+    public Test(){}
+    public Test(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
